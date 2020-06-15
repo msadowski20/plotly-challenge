1ede7ca55180f1d4c1ddfb3f23be90112d3b3cc4
Matt Sadowski <matt.sadowski@gmail.com>
2020-06-14 23:58:14 -0400

    Third commit, updated app.js and index.html files.

--- Belly_Button_Biodiversity/app.js
+++ Belly_Button_Biodiversity/app.js
@@ -1,136 +1,137 @@
-function buildPlots(entry) {
-    d3.json("data/samples.json").then((data) => {
-    var sampleData = data.samples.map(row => row.sample_values);
-    var ids = data.samples.map(row => row.otu_ids);
-    var labels = data.samples.map(row => row.otu_labels);
-    var wfreq = data.metadata.map(row => row.wfreq);
+// Function to build all plots
+function buildPlots(id) {
+    d3.json("data/samples.json").then((data)=> {
+  
+        var wfreq = data.metadata.map(d => d.wfreq)
+        var samples = data.samples.filter(s => s.id.toString() === id)[0];
+        var sampleData = samples.sample_values.slice(0, 10).reverse();
+        var toptenOTU = (samples.otu_ids.slice(0, 10)).reverse();
+        var otuID = toptenOTU.map(d => "OTU " + d);
+        var labels = samples.otu_labels.slice(0, 10);
 
-    var topTenSamples = sampleData.map(row => row.slice(0, 10));
-    var topTenIds = ids.map(row => row.slice(0,10));
-    var topTenLabels = labels.map(row => row.slice(0, 10));
+        // Create Bar Chart
+        var traceBar = {
+            x: sampleData,
+            y: otuID,
+            text: labels,
+            marker: {
+              color: "rgb(51, 115, 167)"
+              },
+            type:"bar",
+            orientation: "h",
+        };
 
-    // console.log(wfreq);
-    // console.log(sampleData);
-    // console.log(ids);
-    // console.log(labels);
-    // console.log(topTenSamples[0]);
-    // console.log(topTenIds[0]);
-    // console.log(topTenLabels[0]);
+        var dataBar = [traceBar];
 
-    // function init() {
-    //     data = [{
-    //         x: topTenSamples[0],
-    //         y: topTenIds[0],
-    //         type: "bar",
-    //         orientation: "h"}]
-        
-    //     Plotly.newPlot("bar", data);
-    // }
-
-
-    // Bar Chart
-    var trace1 = {
-        x: topTenSamples[0],
-        y: topTenIds[0],
-        text: topTenLabels[0],
-        marker: {
-            color: "blue",
-            width: 1
-        },
-        type: "bar",
-        orientation: "h"
-    }
-
-    var data1 = [trace1];
-
-    var layout1 = {
-        title: "Top Ten Biodiversities",
-    }
-
-    Plotly.newPlot("bar", data1, layout1);
-
-    // Gauge Chart
-    var data2 = [{
-        domain: {
-            x: [0, 1],
-            y: [0, 1]
-        },
-        value: wfreq[0],
-        title: {
-            text: "Wash Frequency"
-        },
-        type: "indicator",
-        mode: "gauge+number",
-        gauge: {
-            axis: {
-                range: [null, 9]
+        var layoutBar = {
+            title: "Top 10 OTU Per Subject",
+            yaxis:{
+                tickmode:"linear",
             },
-            steps: [{
-                    range: [0, 2],
-                    color: "red"
-                },
-                {
-                    range: [2, 4],
-                    color: "orange"
-                },
-                {
-                    range: [4, 6],
-                    color: "yellow"
-                },
-                {
-                    range: [6, 8],
-                    color: "lime"
-                },
-                {
-                    range: [8, 9],
-                    color: "green"
-                },
-            ]
-        }
-    }];
+            margin: {
+                l: 80,
+                r: 50,
+                t: 80,
+                b: 25
+            }
+        };
 
+        Plotly.newPlot("bar", dataBar, layoutBar);
+      
+        // Create Bubble Chart
+        var traceBubble = {
+            x: samples.otu_ids,
+            y: samples.sample_values,
+            mode: "markers",
+            marker: {
+                size: samples.sample_values,
+                color: samples.otu_ids
+            },
+            text: samples.otu_labels
+  
+        };
 
-    Plotly.newPlot("gauge", data2);
+        var layoutBubble = {
+            xaxis:{
+                title: "OTU ID"
+            },
+            height: 800,
+            width: 1250
+        };
 
-    // Bubble Chart
-    var trace3 = {
-        x: topTenIds[0],
-        y: topTenSamples[0],
-        text: topTenLabels[0],
-        mode: "markers",
-        markers: {
-            size: topTenSamples[0],
-            color: topTenIds[0]
-        }
-    }
+        var dataBubble = [traceBubble];
 
-    var data3 = [trace3];
+        Plotly.newPlot("bubble", dataBubble, layoutBubble); 
+  
+        // Create Gauge Chart
+        var dataGauge = [
+          {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: parseFloat(wfreq),
+          title: { text: `Weekly Washing Frequency ` },
+          type: "indicator",
+          
+          mode: "gauge+number",
+          gauge: { axis: { range: [null, 9] },
+                   steps: [
+                    { range: [0, 2], color: "yellow" },
+                    { range: [2, 4], color: "orange" },
+                    { range: [4, 6], color: "red" },
+                    { range: [6, 8], color: "lime" },
+                    { range: [8, 10], color: "green" },
+                  ]}
+              
+          }
+        ];
+        var layoutGauge = { 
+            width: 600, 
+            height: 500, 
+            margin: { 
+                t: 20, 
+                b: 20, 
+                l: 50, 
+                r: 50 } 
+          };
+        Plotly.newPlot("gauge", dataGauge, layoutGauge);
+      });
+  }  
 
-    var layout3 = {
-        title: "Top Ten Biodiversities",
-    }
-
-    Plotly.newPlot("bubble", data3, layout3);
-
-});
-}
-
-// Function for getting Demographic info
-function getDemographicInfo(entry) {
-    // read the json file to get data
-    d3.json("Data/samples.json").then((data) => {
+// Function to get the demographic info
+function getDemographicInfo(id) {
+    d3.json("data/samples.json").then((data)=> {
 
         var metadata = data.metadata;
+        var demographicData = metadata.filter(m => m.id.toString() === id)[0];
+        var metadataPanel = d3.select("#sample-metadata");
+        
+        metadataPanel.html("");
 
-        var result = metadata.filter(row => row.id.toString() === entry)[0];
-
-        var demographicInfo = d3.select("#sample-metadata");
-
-        demographicInfo.html("");
-
-        Object.entries(result).forEach((key) => {
-            demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+        Object.entries(demographicData).forEach((key) => {   
+                metadataPanel.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
         });
     });
 }
 
+// Functin to update plots on change
+function updatePlots(id) {
+    buildPlots(id);
+    getDemographicInfo(id);
+}
+
+// Function to initialize the page data
+function init() {
+    d3.json("data/samples.json").then((data)=> {
+
+    var dropdown = d3.select("#selDataset");
+
+    data.names.forEach(function(name) {
+        dropdown.append("option").text(name).property("value");
+    });
+
+    buildPlots(data.names[0]);
+    getDemographicInfo(data.names[0]);
+});
+
+}
+
+init();
